53c2b7e9526d9e3a638b7f620acd098602c134b5
damianb <damian.blazejewski@gmail.com>
2013-03-28 22:21:12 +0100

    hello world

---
+++ ScalaHelloWord/src/greeter/Hello.scala
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Damian
+ * Date: 28.03.13
+ * Time: 22:13
+ * To change this template use File | Settings | File Templates.
+ */
+package greeter
+object Hello extends App{
+  println("Hello word!")
+}
+
